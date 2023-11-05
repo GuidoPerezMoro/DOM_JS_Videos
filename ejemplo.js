3b1f1f9ecfdf36671f7dc9a911d6edc0049546fb
Guido Pérez Moro <90873217+GuidoPerezMoro@users.noreply.github.com>
2023-11-05 05:41:05 -0300

    Video 1

---
+++ ejemplo.js
@@ -0,0 +1,58 @@
+//----------------------------VIDEO 1----------------------------
+/*
+// DOCUMENTO
+console.log(document)
+
+
+// SELECTORES
+//Id
+console.log(document.getElementById('cabecera'))
+console.log(document.getElementById('meloinvento')) //null
+//Clase
+console.log(document.getElementsByClassName('border-grey'))
+//Tipo
+console.log(document.getElementsByTagName('li'))
+console.log(document.getElementsByTagName('nav'))
+//Name
+console.log(document.getElementsByName('cabecera'))
+
+
+// SELECTORES QUERY
+//Tipo
+console.log(document.querySelector("li"))
+console.log(document.querySelectorAll("li"))
+//Clase
+console.log(document.querySelector(".border-grey"))
+console.log(document.querySelectorAll(".border-grey"))
+//Id
+console.log(document.querySelector("#header"))
+console.log(document.querySelectorAll("#main")) //No recomendado
+//Name
+console.log(document.querySelector("[name=cabecera]"))
+console.log(document.querySelectorAll("[name=cabecera]"))
+
+
+// ELEMENTOS
+const elemento = document.getElementById("header")
+
+// CONSOLA NAVEGADOR
+//elemento.children
+//document.children
+//elemento.parentElement
+//document.parentElement //null
+
+
+// TIPOS DE LISTAS
+const lista1 = document.getElementsByClassName("border-grey") //Memoria dinámica
+const lista2 = document.querySelectorAll(".border-grey") //Memoria estática
+
+// CONSOLA NAVEGADOR
+//lista1
+//lista2
+
+*/
+
+
+
+
+
